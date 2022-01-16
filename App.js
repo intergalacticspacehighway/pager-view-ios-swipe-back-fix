24b2040c13bf6f857b9666536e7df3c9b84d7da3
Nishan <nishanbende@gmail.com>
2022-01-16 12:48:32 +0530

    before expo eject

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, ListRenderItem } from "react-native";
+import PagerView from "react-native-pager-view";
+import { Tabs } from "react-native-collapsible-tab-view";
+import {
+  NativeViewGestureHandler,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import {
+  useAnimatedScrollHandler,
+  useHandler,
+  useEvent,
+} from "react-native-reanimated";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export function usePagerScrollHandler(handlers, dependencies) {
+  const { context, doDependenciesDiffer } = useHandler(handlers, dependencies);
+  const subscribeForEvents = ["onPageScroll"];
+
+  return useEvent(
+    (event) => {
+      "worklet";
+      const { onPageScroll } = handlers;
+      if (onPageScroll && event.eventName.endsWith("onPageScroll")) {
+        onPageScroll(event, context);
+      }
+    },
+    subscribeForEvents,
+    doDependenciesDiffer
   );
 }
 
+export default MyPager = () => {
+  const handler = usePagerScrollHandler(
+    {
+      onPageScroll: (e) => {
+        console.log("eefe", e);
+      },
+    },
+    []
+  );
+
+  return (
+    <PagerView style={styles.pagerView} initialPage={0} onPageScroll={handler}>
+      <View key="1">
+        <Text>First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+    </PagerView>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  pagerView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
